5511482d14be2f91f85df60758379a201c3c6107
Chen <info@chenwebsites.com>
2021-02-09 23:11:33 -0500

    final

---
+++ index.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta property="og:image" content="https://www.capoeira-raposa.com/wp-content/themes/Capoeira-raposa/center.jpeg"">
+    <meta property="og:image:type" content="image/jpeg">
+    <meta property="og:image:width" content="200">
+    <meta property="og:image:height" content="200">
+    
+    <link rel="shortcut icon" type="image/jpg" href="<?php echo get_template_directory_uri(); ?>/fox.png">
+
+    <title>המרכז הישראלי לקפוארה - לב השרון</title>
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&family=Ubuntu&display=swap" rel="stylesheet">
+
+    <!-- CSS stylesheets -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/style.css">
+
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/22b07c19fa.js" crossorigin="anonymous"></script>
+
+    <!-- Bootstarp Scripts -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
+
+    <!-- JS script -->
+</head>
+<body>
+
+     <!-- Landing Page -->
+    <section id="landingPage">
+ 
+  
+        <div class="container-fluid">
+            <div class="row">
+            <div class="col">
+                    <div class="leftCol">
+                        <h2 class="heading3">הירשמו לשבועיים ניסיון חינם!</h2>
+                        <?php echo do_shortcode('[contact-form-7 id="5" title="Contact-form-2"]'); ?>
+                    </div>
+            
+                </div>   
+            <div class="col">
+                    <h1 class="heading">המרכז הישראלי לקפוארה לב-השרון בהדרכת כרם גבע</h1>
+                </div> 
+        </div>
+
+            <div class="row">
+                <div class="col-lg-12">
+                    
+                    <h2 class="heading2"> 
+                        מרכז ספורט וחינוך איכותי ומקצועי, המעניק מסגרת כייפית, בטוחה ומעצימה, בתחום החברתי והספורטיבי-חינוכי.
+                    </h2>
+                </div>
+            </div>
+        </div>
+        <a button type="button" class="btn btn-secondary btn-lg btn-block btnReg" href="<?php echo get_template_directory_uri(); ?>/sche.png">להורדת לוח זמני האימונים לחץ כאן</button></a>
+    </section>
+
+<!-- What is Capoeira -->
+    <section id="whatIsCapoeira">
+        <div class="container-fluid">
+            <h1 class="heading21">מה כל-כך שווה בקפוארה?</h1>
+           <div class="row">
+               
+             <div class="col">
+                 
+                 <ul class="list-display list-checkmarks dir-rtl">
+                     <img class="foxImg" src="<?php echo get_template_directory_uri(); ?>/fox.png" alt=""><li class="para">רכישת מיומניות תנועתיות בסביבה בטוחה ומקצועית</li>
+                     <br>
+
+                     <img class="foxImg" src="<?php echo get_template_directory_uri(); ?>/fox.png" alt=""><li class="para">עליה בבטחון העצמי ובתחושת המסוגלות
+</li>
+                     <br>
+
+                     <img class="foxImg" src="<?php echo get_template_directory_uri(); ?>/fox.png" alt=""><li class="para">פיתוח הקורדינאציה והיכולות האקרובטיות</li>
+                     <br>
+                     <img class="foxImg" src="<?php echo get_template_directory_uri(); ?>/fox.png" alt=""><li class="para">עבודה בסביבה חינוכית וחוויתית
+</li>
+                     <br>
+                     <img class="foxImg" src="<?php echo get_template_directory_uri(); ?>/fox.png" alt=""><li class="para">יחס אישי ותשומת לב לחיזוק כל ילד
+</li>
+                     <br>
+                     <img class="foxImg" src="<?php echo get_template_directory_uri(); ?>/fox.png" alt=""><li class="para">שעורים בכל ימות השבוע
+</li>
+                     <br>
+                     
+                    </ul> 
+                    
+                </div>
+            </div> 
+
+            <div class="row snifim">
+                <div class="col">
+                    <h3>סניפים</h3>
+                    <ol>
+                        <li>פרדסיה</li>
+                        <li>צור-משה</li>
+                        <li>תנובות</li>
+                    </ol>
+                    <h3>טלפון</h3>
+                    <p>כרם גבע: 054-5838032</p>
+                </div>
+</div>
+            <h2 class="description">ההזדמנות לתת לילדכם חוויה ספורטיבית, מהנה, מאתגרת, ומפתחת יכולות חיוניות.</h2>
+           <a button href="#landingPage" type="button" class="btn btn-warning btn-lg btn-block btnReg">!הירשמו לשבועיים ניסיון חינם</button></a> 
+        </div>            
+    </section>
+
+<section id="onUs">
+    <div class="container-fluid">
+        <h2 class="headingOnUs">קצת עלינו</h2>
+        
+            
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/carousel1.jpeg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/carousel2.jpeg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/carousel3.jpeg" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/carousel4.jpeg" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="<?php echo get_template_directory_uri(); ?>/carousel5.jpeg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+            
+            
+                <p class="paraOnUs">המרכז הישראלי לקפוארה בפרדסיה, צור-משה ותנובות פועל במספר סניפים ברחבי לב-השרון.
+                     המרכז הוא אחד מסניפיו של המרכז הישראלי לקפוארה,
+                      מבתי הספר המובילים בארץ ובעולם לקפוארה, עם וותק של למעלה מ-25 שנה בישראל.
+                       המרכז הישראלי לקפוארה שואף להיות בית חינוך איכותי ומקצועי, המעניק מסגרת לימודית, חינוכית וחברתית בסביבה בטוחה ומאתגרת.
+                        כיום משפחת המרכז הישראלי כולל סנפיים רבים ברחבי הארץ וכן סניפים ב: קנדה, ארה״ב, איטליה, רוסיה, גרמניה והודו.
+                        <br>
+                        <br>
+                        העובדה שבית-הספר שלנו מוכר לא רק בכל הארץ אלא גם בכל העולם,
+                        ושכל צוות המדריכים מוסמך ע”י מכון וינגייט לספורט,
+                        עובר השתלמויות מגוונות בברזיל והכשרות ייחודיות בתחום הקואורדינציה
+                        והעבודה עם הגיל הרך אמורה לתת מושג עד כמה אנחנו רציניים בלספק את חוויית האימון הכי טובה שיש.
+                        נשמע לכם טוב? זה מה שחיפשתם? נשמח לקבל אתכם למשפחה שלנו. קפצו לשיעור ניסיון חינם בכל יום ובכל שעה.
+
+                        </p>
+            
+        
+    </div>
+    <p style="text-align: center">
+    <iframe width="360" height="260" src="https://www.youtube.com/embed/8Znq4usRghY" 
+    frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </p>
+    
+</section>
+
+<!-- Footer -->
+
+
+</body>
+</html>
